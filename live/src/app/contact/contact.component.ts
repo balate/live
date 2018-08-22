c7d2d3f94e1de35c4e144f476110afe8bc795c07
Javier Contreras <javi_ocon@yahoo.es>
2018-08-22 18:02:51 +0200

    generate module and component to contact

---
+++ live/src/app/contact/contact.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kc-contact',
+  template: `
+    <p>
+      contact works!
+    </p>
+  `,
+  styles: []
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
