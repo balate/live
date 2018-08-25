c40aa1030f04d7eb133c6ec46ba4f7e9ba859a9d
Javier Contreras <javi_ocon@yahoo.es>
2018-08-25 19:39:24 +0200

    configure greet component

--- live/src/app/home/home.module.ts
+++ live/src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { GreetComponent } from './greet/greet.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, GreetComponent]
 })
 export class HomeModule { }
