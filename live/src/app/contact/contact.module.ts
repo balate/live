77f3fd5b0764e1360ce644f15bedc87026562f34
Javier Contreras <javi_ocon@yahoo.es>
2018-08-31 20:09:15 +0200

    add formsModule

--- live/src/app/contact/contact.module.ts
+++ live/src/app/contact/contact.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
 import { FormVdComponent } from './form-vd/form-vd.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     ContactRoutingModule
   ],
   declarations: [ContactComponent, FormVdComponent]
