37dbcd4975987b025bb95d72c1afa5c9f4a51428
Javier Contreras <javi_ocon@yahoo.es>
2018-08-31 19:13:23 +0200

    create form in contact

--- live/src/app/contact/contact.module.ts
+++ live/src/app/contact/contact.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact.component';
+import { FormVdComponent } from './form-vd/form-vd.component';
 
 @NgModule({
   imports: [
     CommonModule,
     ContactRoutingModule
   ],
-  declarations: [ContactComponent]
+  declarations: [ContactComponent, FormVdComponent]
 })
 export class ContactModule { }
